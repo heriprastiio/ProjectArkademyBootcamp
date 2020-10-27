e34c1d03d87d5a936684a0acb53ac420f2d04192
heriprastiio <heri.prastio56@gmail.com>
2020-10-27 13:29:59 +0700

    Initial commit

--- src/main/kotlin/tugasSatu/Biodata.kt
+++ src/main/kotlin/tugasSatu/Biodata.kt
@@ -1,6 +1,6 @@
 package tugasSatu
 
 //Level 1 Tugas 1
-fun levelSatuTugasSatu(){
+fun main(){
     println("Nama anda adalah: Heri Prastio \nUmur: 25 tahun\n Alamat: Jl. Duri Barat Hobi: Berenang \nAsal universitas: Universitas Esa Unggul\n Kesulitan Belajar Programming: Sering ada error pusing \nAlasan Ingin Ikut Bootcamp Arkademy Karena dengan adanya bootcamp ini, saya dibimbing lebih mendalam membantu mengerjakan real projek")
 }
